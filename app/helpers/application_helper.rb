064881aad5cbf7dac3a081efde62a33ade2cef2f
Josh <jjmoona0@gmail.com>
2018-07-28 22:29:14 +1200

    Added bootstrap and completed base static pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  #Returns the full title on the page
+  def full_title(page_title = '')
+    base_title = "Profile Page"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
